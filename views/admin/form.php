d502f78d2a88505ea1d602010ae639e1e48be155
Adam Fairholm <adam.fairholm@gmail.com>
2012-01-08 20:13:11 -0500

    Updating to the new form format.

--- views/admin/form.php
+++ views/admin/form.php
@@ -6,19 +6,22 @@
 <section class="item">
 
 	<?php echo form_open_multipart($this->uri->uri_string(), 'class="crud"'); ?>
-		<ul class="form_inputs">
+		
+		<div class="form_inputs">
+	
+		<ul>
 			<li class="<?php echo alternator('', 'even'); ?>">
-				<label for="name"><?php echo lang('sample.name'); ?></label>
-				<?php echo form_input('name', set_value('name', $name), 'class="width-15"'); ?>
-				<span class="required-icon tooltip">Required</span>
+				<label for="name"><?php echo lang('sample.name'); ?> <span>*</span></label>
+				<div class="input"><?php echo form_input('name', set_value('name', $name), 'class="width-15"'); ?></div>
 			</li>
 
 			<li class="<?php echo alternator('', 'even'); ?>">
-				<label for="slug"><?php echo lang('sample.slug'); ?></label>
-				<?php echo form_input('slug', set_value('slug', $slug), 'class="width-15"'); ?>
-				<span class="required-icon tooltip">Required</span>
+				<label for="slug"><?php echo lang('sample.slug'); ?> <span>*</span></label>
+				<div class="input"><?php echo form_input('slug', set_value('slug', $slug), 'class="width-15"'); ?></div>
 			</li>
 		</ul>
+		
+		</div>
 		
 		<div class="buttons">
 			<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save', 'cancel') )); ?>
